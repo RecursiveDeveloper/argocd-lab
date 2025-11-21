84699f9bee50dd7c4a52c1d6edee7318eb01ec86
jhjortiz <jhjortiz@bancolombia.com.co>
2025-11-21 09:52:35 -0500

    Exposed ports updated to meet argocd definition. Additional comments modified to access argocd app on localhost

--- Vagrantfile
+++ Vagrantfile
@@ -4,8 +4,7 @@ Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/jammy64"
   config.vm.hostname = "argocd-lab"
   config.vm.network "forwarded_port", guest: 80, host: 80
-  config.vm.network "forwarded_port", guest: 3000, host: 3000
-  config.vm.network "forwarded_port", guest: 5000, host: 5000
+  config.vm.network "forwarded_port", guest: 443, host: 443
   config.vm.synced_folder "k8s-manifests", "/opt/k8s-manifests"
 
   config.vm.provider "virtualbox" do |vb|
--- scripts/install_argocd.sh
+++ scripts/install_argocd.sh
@@ -10,5 +10,6 @@ microk8s kubectl apply -f /opt/k8s-manifests/ingress.yml
 
 admin_encoded_password=$(microk8s kubectl get secret argocd-initial-admin-secret -n ${namespace} -o jsonpath="{.data.password}")
 admin_password=$(echo ${admin_encoded_password} | base64 --decode)
+echo -e "\nTo access ArgoCD UI, visit https://localhost"
+echo -e "\nArgoCD username: admin"
 echo -e "\nArgoCD admin password: ${admin_password}"
-echo -e "\nTo access ArgoCD UI, visit http://localhost:8080"
